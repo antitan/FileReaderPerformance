63014cc08d1a3f83843772d3f07fce66a1f09d10
antitan <40539719+antitan@users.noreply.github.com>
2024-11-11 10:26:37 +0100

    Update BlumbergFileReaderService.cs

--- BlumbergFileReaderService.cs
+++ BlumbergFileReaderService.cs
@@ -7,7 +7,7 @@ Lorsqu’une nouvelle requête web est envoyée au travers de la méthode Paymen
 3)	On itère sur les résultats d’exécution de chaque règle :
 -	Si le résultat de l’exécution de la règle courante est IsSuccess=true :
 -Si le résultat attendu de la règle est de type challenge  -> alors il y a challenge on insert un enregistrement dans ActivityWorkflow avec la valeur 0 puisque la règle n’est pas passée.
--Sinon si le resultat est de type Allow  on insert un enregistrement dans ActivityWorkflow avec la valeur 1 puisque la règle est pas passée.
+-Sinon si le resultat est de type Allow  on insert un enregistrement dans ActivityWorkflow avec la valeur 1 puisque la règle est passée.
               -     Sinon Si le résultat de l’exécution de la règle courante est IsSuccess=false :
 	    - Si le résultat attendu de la règle est de type challenge  -> alors il y a challenge on insert un enregistrement dans ActivityWorkflow avec la valeur 0 puisque la règle n’est pas passée.
 	    - Sinon si le résultat est de type Allow on insert on insert un enregistrement dans ActivityWorkflow avec la valeur 0 puisque la règle est passée.
